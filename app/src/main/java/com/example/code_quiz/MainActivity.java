93c9f0b22dd3afa604e86cd81ba5519fbe1bf7fe
LazyPanda397 <gauravr397@gmail.com>
2022-06-19 16:54:05 +0530

    score_sheet -->

--- app/src/main/java/com/example/code_quiz/MainActivity.java
+++ app/src/main/java/com/example/code_quiz/MainActivity.java
@@ -106,6 +106,15 @@ public class MainActivity extends AppCompatActivity {
         quizmodalArrayList.add(new Quizmodal(" Which statement is true about Java?","Java is a sequence-dependent programming language","Java is a code dependent programming language","Java is a platform-dependent programming language","Java is a platform-independent programming language","Java is a platform-independent programming language"));
         quizmodalArrayList.add(new Quizmodal("Which component is used to compile, debug and execute the java programs?","JRE","JIT"," JDK","JVM"," JDK"));
         quizmodalArrayList.add(new Quizmodal("Which one of the following is not a Java feature?"," Object-oriented"," Use of pointers","Portable","Dynamic and Extensible"," Use of pointers"));
+        quizmodalArrayList.add(new Quizmodal("Which of these cannot be used for a variable name in Java?","identifier & keyword","identifier"," keyword","none of the mentioned"," keyword"));
+        quizmodalArrayList.add(new Quizmodal("What is the extension of java code files?",".js",".txt",".class",".java",".java"));
+        quizmodalArrayList.add(new Quizmodal("Which environment variable is used to set the java path?","MAVEN_Path"," JavaPATH"," JAVA"," JAVA_HOME"," AVA_HOME"));
+        quizmodalArrayList.add(new Quizmodal( "Which of the following is not an OOPS concept in Java?","Polymorphism","Inheritance"," Compilation"," Encapsulation"," Compilation"));
+        quizmodalArrayList.add(new Quizmodal("What is not the use of “this” keyword in Java?","Referring to the instance variable when a local variable has the same name"," Passing itself to the method of the same class","Passing itself to another method","Calling another constructor in constructor chaining"," Passing itself to the method of the same class"));
+        quizmodalArrayList.add(new Quizmodal("What will be the error in the following Java code?\n" +
+                "\n" +
+                "    byte b = 50;\n" +
+                "    b = b * 50;"," b cannot contain value 50"," b cannot contain value 100, limited by its range","No error in this code","* operator has converted b * 50 into int, which can not be converted to byte without casting","* operator has converted b * 50 into int, which can not be converted to byte without casting"));
 
     }
 }
